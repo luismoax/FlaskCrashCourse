162a758588c92d5bd3052d2546ab2d505fe6b9d3
Luis Manuel <luismoax@gmail.com>
2020-05-14 00:52:37 -0400

    add two routes one with a onlyget function (allowing only GET) and another onlypost function (allowing only POST)

--- app.py
+++ app.py
@@ -5,8 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home/users/<string:name>/posts/<int:id>')
-def hello(id, name):
-    return "Hello, " + name + "your id is: " + str(id)
+def hello(id = -1, name = 'default'):
+    return "Hello, " + name + " your id is: " + str(id)
+
+@app.route('/onlyget', methods = ['GET']) 
+def get_req():
+    return "You can only get this webpage."
+
+@app.route('/onlypost', methods = ['POST'])
+def only_post():
+    return "This won't be displayed"
 
 if __name__ == "__main__":
     app.run(debug=True)
