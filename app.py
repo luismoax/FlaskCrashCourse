faa9f3fd69f4058c5aa2d30facc86f9dae733333
Luis Manuel <luismoax@gmail.com>
2020-05-14 21:52:14 -0400

    create a post view, and print dummy data about post passing them in a variable to the render_template method. use for and if on view

--- app.py
+++ app.py
@@ -3,10 +3,33 @@ from flask import render_template
 
 app = Flask(__name__)
 
+all_posts = [
+    {
+        'title': 'Amorphis Post',
+        'content': 'This is the content of post 1',
+        'author': 'Luismo'
+    },
+    {
+        'title': 'In Flames Post',
+        'content': 'Testing this awesome micro-framework'
+    },
+    {
+        'title': 'Lorem Post',
+        'content': '''Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Aliquid voluptatum quia quaerat necessitatibus fugiat tenetur nostrum! 
+                      Suscipit sapiente quam quia. Repudiandae eos, cumque quas 
+                      atque aut amet corporis distinctio enim.'''
+    }
+]
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts = all_posts)
 
 @app.route('/home/users/<string:name>/posts/<int:id>')
 def hello(id = -1, name = 'default'):
