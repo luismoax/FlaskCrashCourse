107a84970a73fc4d325b9a04cc6257c8dec75267
Luis Manuel <luismoax@gmail.com>
2020-07-22 23:18:17 -0400

    add a navbar to base.html

--- app.py
+++ app.py
@@ -10,30 +10,36 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///posts.db"
 db = SQLAlchemy(app)
 
 # designing the model
+
+
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.String(500), nullable=False)
     author = db.Column(db.String(50), nullable=False, default='N/A')
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    
-    def __repr__(self): 
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+
+    def __repr__(self):
         return 'Blog Post ' + str(self.id)
 
+@app.route('/home', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
+
 @app.route('/posts', methods=['GET', 'POST'])
-def posts():    
+def posts():
     # if is a post request
-    if request.method == 'POST':        
+    if request.method == 'POST':
         post_title = request.form['title']
         post_content = request.form['content']
         post_author = request.form['author']
-        
+
         # create the object
-        new_post = BlogPost(title = post_title, content = post_content, author = post_author)
+        new_post = BlogPost(
+            title=post_title, content=post_content, author=post_author)
         # add to the db
         db.session.add(new_post)
         # commit the change
@@ -42,9 +48,10 @@ def posts():
     else:
         # get all posts added so far
         added_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
-        
-        return render_template('posts.html', posts = added_posts)
-    
+
+        return render_template('posts.html', posts=added_posts)
+
+
 @app.route('/posts/delete/<int:id>')
 def delete(id):
     post_to_delete = BlogPost.query.get_or_404(id)
@@ -52,8 +59,9 @@ def delete(id):
     db.session.commit()
     return redirect('/posts')
 
-@app.route('/posts/edit/<int:id>', methods = ['POST', 'GET'] )
-def edit(id):    
+
+@app.route('/posts/edit/<int:id>', methods=['POST', 'GET'])
+def edit(id):
     if request.method == 'POST':
         post_to_edit = BlogPost.query.get_or_404(id)
         post_to_edit.title = request.form['title']
@@ -63,20 +71,23 @@ def edit(id):
         return redirect('/posts')
     else:
         post_to_edit = BlogPost.query.get_or_404(id)
-        return render_template('edit.html', post = post_to_edit)
-        
+        return render_template('edit.html', post=post_to_edit)
+
 
 @app.route('/home/users/<string:name>/posts/<int:id>')
-def hello(id = -1, name = 'default'):
+def hello(id=-1, name='default'):
     return "Hello, " + name + " your id is: " + str(id)
 
-@app.route('/onlyget', methods = ['GET']) 
+
+@app.route('/onlyget', methods=['GET'])
 def get_req():
     return "You can only get this webpage."
 
-@app.route('/onlypost', methods = ['POST'])
+
+@app.route('/onlypost', methods=['POST'])
 def only_post():
     return "This won't be displayed"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
